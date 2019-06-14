8082596e0e635773097dced970281175cf1e02b8
ZIDAT <zidatsara@gmail.com>
2019-06-14 22:47:58 +0200

    WordsCount code

---
+++ src/WordsCount.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordsCount {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        ArrayList<String> text = new ArrayList<>();
+        text.add("bonjour");
+        text.add("je");
+        text.add("bonjour");
+        Map<String, Integer> countWords = new HashMap<>();
+        /*for(String key : text){
+            map.put (key,0);
+        }*/
+        /*for (String key : text){
+            key = text[0];
+
+        }*/
+
+
+    }
+}
